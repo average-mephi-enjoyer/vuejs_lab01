1b790be32062e41b0a0b9ba4f62c54b1f1f0309c
Yashchenko Artem <artemyaschenko@inbox.ru>
2026-02-28 11:22:11 +0300

    dialogs added in task2

--- task2.js
+++ task2.js
@@ -1,6 +1,6 @@
+// ИНСТРУМЕНТЫ
 // task2_1 array
 const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
-
 // task2_4 class
 const Counter = {
     count: 0,
@@ -36,64 +36,99 @@ function get_first_primes(n) {
 
 //task2_6
 function is_palindrome(str) {
-    const cleaned = str.replace(/[^a-zA-Zа-яёА-ЯЁ0-9]/g, '').toLowerCase();
-    const reversed = cleaned.split('').reverse().join('');
+    let cleaned = str.replace(/[^a-zA-Zа-яёА-ЯЁ0-9]/g, '').toLowerCase();
+    let reversed = cleaned.split('').reverse().join('');
     return cleaned === reversed;
 }
 
-//task2_2
-let input = prompt("Введите номер месяца (1-12)");
-if (input != null) {
-    let monthNumber = parseInt(input, 10);
-    if (!isNaN(monthNumber) && Number.isInteger(monthNumber) && monthNumber >= 1 && monthNumber <= 12) alert(months[monthNumber - 1]);
-    else alert("Некорректный номер месяца");
+
+// ДИАЛОГИ
+// task2_2
+function dialog_months(){
+    let input = prompt("Введите номер месяца (1-12)");
+    if (input != null) {
+        let monthNumber = parseInt(input, 10);
+        if (!isNaN(monthNumber) && Number.isInteger(monthNumber) && monthNumber >= 1 && monthNumber <= 12) alert(months[monthNumber - 1]);
+        else alert("Некорректный номер месяца");
+    }
 }
 
 // task2_3
-input = prompt("Введите количество простых чисел");
-if (input != null){
-    let n = parseInt(input, 10);
-    if (isNaN(n) || !Number.isInteger(n) || n <= 0) alert("Некорректный ввод!");
-    else {
-        let primes = get_first_primes(n);
-        alert(primes.join(" "));
+function dialog_primes(){
+    let input = prompt("Введите количество простых чисел");
+    if (input != null){
+        let n = parseInt(input, 10);
+        if (isNaN(n) || !Number.isInteger(n) || n <= 0) alert("Некорректный ввод!");
+        else {
+            let primes = get_first_primes(n);
+            alert(primes.join(" "));
+        }
     }
 }
 
 // task2_4
-alert("Начальное значение: " + Counter.count);
-input = prompt("Введите число для добавления");
-if (input !== null) {
-    let add_value = parseFloat(input);
-    if (!isNaN(add_value)) {
-        Counter.add(add_value);
-        alert("Результат: " + Counter.count);
-    }
-    else alert("Некорректный ввод!");
-} 
-else alert("Операция отменена!");
-input = prompt("Введите число для вычитания");
-if (input !== null) {
+function dialog_counter(){
+    alert("Начальное значение: " + Counter.count);
+    let input = prompt("Введите число для добавления");
+    if (input !== null) {
+        let add_value = parseFloat(input);
+        if (!isNaN(add_value)) {
+            Counter.add(add_value);
+            alert("Результат: " + Counter.count);
+        }
+        else alert("Некорректный ввод!");
+    } 
+    else alert("Операция отменена!");
+
+    input = prompt("Введите число для вычитания");
+    if (input !== null) {
     let sub_value = parseFloat(input);
     if (!isNaN(sub_value)) {
         Counter.sub(sub_value);
         alert("Результат: " + Counter.count);
     }
     else alert("Некорректный ввод!");
-} 
-else alert("Операция отменена!");
+    } 
+    else alert("Операция отменена!");
+}
 
 // task2_5
-input = prompt("Введите слова через запятую");
-if (input === null) alert("Ввод отменён.");
-else if (input.trim() === "") alert("Вы ничего не ввели.");
-else alert("Результат: " + input.replace(/,/g, '.'));
+function dialog_sep(){
+    let input = prompt("Введите слова через запятую");
+    if (input === null) alert("Ввод отменён.");
+    else if (input.trim() === "") alert("Вы ничего не ввели.");
+    else alert("Результат: " + input.replace(/,/g, '.'));
+}
 
 // task2_6
-input = prompt("Введите строку, проверим, палиндром ли она");
-if (input != null) {
-    if (input === "") alert("Пустая строка");
-    else if (is_palindrome(input)) alert("Да");
-    else alert("Нет");
+function dialog_pal(){
+    input = prompt("Введите строку, проверим, палиндром ли она");
+    if (input != null) {
+        if (input === "") alert("Пустая строка");
+        else if (is_palindrome(input)) alert("Да");
+        else alert("Нет");
+    }
+    else alert("Ввод отменён.");
 }
-else alert("Ввод отменён.");
+
+
+// НАЧАЛО ВЫПОЛНЕНИЯ ЗДЕСЬ!!!
+while (true) {
+    let choice = prompt(`
+        Выберите задание:
+        1: выбор месяца по номеру;
+        2: вывод простых чисел;
+        3: работа с объектом Counter;
+        4: разделение слов точками;
+        5: проверка слова на палиндром;
+        0: завершить работу со страницей;
+      `);
+    if (choice === null) break;
+    choice = choice.trim();
+    if (choice === "") continue;
+    if (choice === "0") break;
+
+    const tasks = { 1: dialog_months, 2: dialog_primes, 3: dialog_counter, 4: dialog_sep, 5: dialog_pal };
+    if (tasks[choice]) tasks[choice]();
+    else alert("Некорректный выбор!");
+  }
